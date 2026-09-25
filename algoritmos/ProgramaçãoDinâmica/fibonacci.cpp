4d744bc983ecf04f2a5d769081764284d5ecb1a1
lopraoliv <lopraoliv@gmail.com>
2026-09-25 00:30:26 +0000

    Sla

---
+++ algoritmos/ProgramaçãoDinâmica/fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Fibonacci(int a);
+
+int main(void){
+    int N;
+    long long int f;
+    cin >> N;
+
+    f = Fibonacci(N);
+    cout << f << "\n";
+
+    return 0;
+}
+
+int Fibonacci(int a){
+    if(a==0){
+        return 0;
+    }else if((a == 1) || (a == 2)){
+        return 1;
+    }else{
+        return (Fibonacci(N-1) + Fibonacci(N-2));
+    }
+}
